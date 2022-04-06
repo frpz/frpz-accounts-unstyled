0ca57a7ed2520a2c3edaceaab93e9b7c74401ea7
Francis Pugnère <francis@frpz.fr>
2022-04-06 19:17:52 +0200

    update version

---
+++ README.md
@@ -0,0 +1,3 @@
+# frpz:accounts-unstyled
+
+fork from (https://github.com/royGil/accounts-unstyled)[https://github.com/royGil/accounts-unstyled] because it was not maintained anymore.
--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   summary: 'Semantic UI Components for Useraccounts-React',
   version: '1.0.0',
-  name: 'meteoreact:accounts-unstyled',
+  name: 'frpz:accounts-unstyled',
   git: 'https://github.com/royGil/useraccounts-react.git'
 })
 
 Package.onUse(api => {
-  api.versionsFrom('1.6.1')
+  api.versionsFrom('2.7.1')
 
   api.use('ecmascript')
   api.mainModule('index.js', ['client', 'server'])
